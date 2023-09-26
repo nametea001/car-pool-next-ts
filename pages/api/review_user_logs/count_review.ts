70ca7e196a8fda535598f444753e096f4903a9fe
Nontakorn Konkain <nonami798@gmail.com>
2023-09-27 01:32:51 +0700

    count Review Log

---
+++ pages/api/review_user_logs/count_review.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { JWT } from "../../../src/Auth/JWT";
+import { ReviewUserLogFinder } from "../../../src/Domain/ReviewUserLog/Service/ReviewUserLogFinder";
+
+export default async function getPosts(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // const dataParam: any = req.query;
+  // const dataBody: any = req.body;
+  let viewData: any = {};
+  const jwt = new JWT();
+  const token = req.headers["auth-token"];
+  const tokenVerify: any = jwt.verifyToken(token);
+  if (req.method == "GET" && tokenVerify) {
+    const reviewUserLogFinder = new ReviewUserLogFinder();
+    let reviewUserlog = await reviewUserLogFinder.countReviewUserLogs(
+      tokenVerify.id
+    );
+    if (reviewUserlog) {
+      viewData.message = "Get Review User Log Successful";
+      viewData.error = false;
+      viewData.review_user_log = reviewUserlog;
+      res.status(200).send(viewData);
+    } else {
+      res.status(401).send("err null data");
+    }
+  } else {
+    res.status(400).send("Bad request");
+  }
+  res.end();
+}
